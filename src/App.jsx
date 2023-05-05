e11b62fbc8e322756bd4cb7df4520099dfc7ab11
borgesmj <borgesmj19@gmail.com>
2023-05-05 10:20:35 -0500

    añadiendo detalles de interfaz

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,23 @@ function App() {
   const [answer, setAnswer] = useState(0)
 
   return (
-    <div className='bg-red-600 flex flex-col m-auto w-full md:w-2/5 xl:w-1/5'>
-        <Pantalla
-          expression={expression}
-          setExpression={setExpression}
-          answer={answer}
-          setAnser={setAnswer}
-        />
-        <Teclado
-          expression={expression}
-          setExpression={setExpression}
-          answer={answer}
-          setAnser={setAnswer}
-        />
+    <div className='w-full lg:w-2/5 xl:w-1/4 bg-red-500 rounded-lg m-auto shadow-xl shadow-gray-300'>
+        <div className='flex flex-col  w-4/5  m-auto'>
+            <Pantalla
+                expression={expression}
+                setExpression={setExpression}
+                answer={answer}
+                setAnser={setAnswer}
+            />
+            <Teclado
+                expression={expression}
+                setExpression={setExpression}
+                answer={answer}
+                setAnser={setAnswer}
+            />
+        </div>
     </div>
+    
   )
 }
 
